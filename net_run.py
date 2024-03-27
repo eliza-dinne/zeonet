357d3c80b6a298790ef2a43e2318247a6d9d8f92
nicoletti901 <ebdinne@inbox.lv>
2024-03-27 23:40:30 +0100

    Fixed how net_run.py writes zeonet_train.sh

--- net_run.py
+++ net_run.py
@@ -6,12 +6,8 @@ import os
 #alignn 2,4,6 layers
 #cgcn same nr of primary layers, 0 alignn
 
-for ch in [32]: #32, 64, 128, 256
+for ch in [64]: #32, 64, 128, 256
     for interactions in [2, 4, 6]: #2, 4, 6
-        with open('zeonet_train.sh', 'r') as file:
-            lines = file.readlines()
-
-            for j in range(len(lines)):
-                if lines[j].startswith('python schnet_test.py'):
-                    lines[j] = f'python schnet_test.py --h {ch} --i {interactions}\n'
+        with open('zeonet_train.sh', 'w') as file:
+            file.write(f'#!/bin/bash\nsource /home/TUE/20183707/miniconda3/etc/profile.d/conda.sh\nsource activate env1\npython schnet_test.py --h {ch} --i {interactions}\nsource deactivate')
         os.system(f'sbatch -p all --gres=gpu:1 zeonet_train.sh')
--- zeonet_train.sh
+++ zeonet_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /home/TUE/20183707/miniconda3/etc/profile.d/conda.sh
 source activate env1
-python schnet_test.py --h 32 --i 4
+python schnet_test.py --h 64 --i 4
 source deactivate
